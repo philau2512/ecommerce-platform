5554d06c08e6e45d4104f535de82295f152caf09
HungLe <hung0964153699@gmail.com>
2025-07-04 14:11:01 +0700

    homepage setup and a bit of detail

--- src/main/java/com/example/ecommerceplatform/controller/HomeController.java
+++ src/main/java/com/example/ecommerceplatform/controller/HomeController.java
@@ -1,13 +1,38 @@
 package com.example.ecommerceplatform.controller;
 
+import com.example.ecommerceplatform.model.Product;
+import com.example.ecommerceplatform.repository.IProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+    @Autowired
+    private IProductRepository productRepository;
+
     @RequestMapping("")
-    public String home() {
+    public String home(@RequestParam(defaultValue = "0") int page, Model model) {
+        Page<Product> products = productRepository.findAll(PageRequest.of(page, 8));
+        model.addAttribute("products", products);
         return "home";
+    }
+
+    @RequestMapping("/products/{id}")
+    public String productDetail(@PathVariable Long id, Model model) {
+        Product product = productRepository.findById(id).orElse(null);
+        model.addAttribute("product", product);
+        return "product/detail";
     }
 }
--- src/main/java/com/example/ecommerceplatform/repository/IProductRepository.java
+++ src/main/java/com/example/ecommerceplatform/repository/IProductRepository.java
@@ -1,6 +1,7 @@
 package com.example.ecommerceplatform.repository;
 
 import com.example.ecommerceplatform.model.Product;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IProductRepository extends JpaRepository<Product, Long> {
